e4f5bda44b7fe9ea7ce4b1bbdddab04ff6092480
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-05-22 20:37:37 -0700

    Comment LineGroupList.cpp

--- DamMap/LineGroupList.cpp
+++ DamMap/LineGroupList.cpp
@@ -11,14 +11,17 @@ LineGroupList::LineGroupList(Frame& fr)
 	//sorting linesP from longest to shortest line
 	sort(linesP.begin(), linesP.end(), compareLine);
 
+	//iterate through lines detected by probabilistic Hough detector
 	while (linesP.size() > 0) {
 		vector<Vec4i> lineGroup;
 		Vec4i lineOfInterest;
+		//Move line from linesP to lineGroup
 		for (size_t i = 0; i < 4; i++) {
 			lineOfInterest[i] = linesP[0][i];
 		}
 		lineGroup.push_back(lineOfInterest);
 		linesP.erase(linesP.begin());
+		//Search for similar lines in linesP to try to group them together with lineOfInterest
 		for (auto it = linesP.begin(); it != linesP.end(); ) {
 			if (linesAreSimilar(*it, lineOfInterest)) {
 				Vec4i newLine;
@@ -32,6 +35,7 @@ LineGroupList::LineGroupList(Frame& fr)
 				++it;
 			}
 		}
+		//Store a group of lines that are similar to each other
 		groupsOfLines.push_back(lineGroup);
 	}
 }
@@ -40,19 +44,17 @@ LineGroupList::~LineGroupList()
 {
 }
 
-
+//Show groups of lines (for debugging)
 void LineGroupList::showGroups() {
 	Mat img;
-	frame->getSource().copyTo(img);
+	frame->getSource().copyTo(img); //Separate copy to avoid changing the source image
 	for (auto it = groupsOfLines.begin(); it != groupsOfLines.end(); it++) {
+		//Give each line group a random color, so that it can be seen that they are correctly grouped together
 		Scalar color = Scalar(rand() % 255, rand() % 255, rand() % 255);
 		for (auto j = (*it).begin(); j != (*it).end(); j++) {
+			//Draw each line in the line group
 			line(img, Point((*j)[0], (*j)[1]), Point((*j)[2], (*j)[3]), color, 1, 1);
 		}
-		//LineSimplified l = LineSimplified(*it);
-		//cout << "Vertical: " << l.getVertical() << " DistFromTopLeft: " << l.getDistFromTopLeft() << endl;
-		//Vec4i endPointL = l.getEndpoints(img.rows, img.cols);
-		//line(img, Point(endPointL[0], endPointL[1]), Point(endPointL[2], endPointL[3]), color, 3, 1);
 	}
 	imshow("image with hough lines p", img);
 }
