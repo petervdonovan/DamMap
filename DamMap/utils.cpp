439d28be7c722363c9523222fb5f14977c51f895
petervdonovan <33707478+petervdonovan@users.noreply.github.com>
2019-05-23 16:39:41 -0700

    Update utils.cpp

--- DamMap/utils.cpp
+++ DamMap/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.h"
 
+//Fast version of root function.
+//Borrowed from http://supp.iar.com/FilesPublic/SUPPORT/000419/AN-G-002.pdf
 unsigned int root(unsigned int x) {
 	unsigned int a, b;
 	b = x;
@@ -13,24 +15,26 @@ unsigned int root(unsigned int x) {
 	return(x);
 }
 
+//Get distance from x and y coordinates
 int distance(int x0, int y0, int x1, int y1) {
 	return root((x1 - x0)*(x1 - x0) + (y1 - y0)*(y1 - y0));
 }
 
-
+//Get angle of a line stored as a cv::Vec4i (vector of four integers)
 double angleOfLine(Vec4i line) {
 	if ((line[2] - line[0]) == 0) return CV_PI / 2;
 	return atan((line[3] - line[1]) / (line[2] - line[0]));
 }
 
+//Compare the lengths of two lines. Used for sorting by length
 bool compareLine(Vec4i line0, Vec4i line1) {
 	return distance(line0[0], line0[1], line0[2], line0[3]) > distance(line1[0], line1[1], line1[2], line1[3]);
 }
 
-
+//Returns a maxumum value of a sample of a specified channel in a region of interest
 int getAvgChannelNearby(const Mat& roi, int channel, int iterations) {
 	int sum = 0;
-	for (int i = 0; i < iterations; i++) {
+	for (int i = 0; i < iterations; i++) { //Iterations is sample size, adjusted based on time constraint
 		int col = rand() % roi.cols;
 		int row = rand() % roi.rows;
 		sum += roi.at<Vec3b>(row, col)[channel];
@@ -38,12 +42,13 @@ int getAvgChannelNearby(const Mat& roi, int channel, int iterations) {
 	return sum / iterations;
 }
 
+//Returns the maximum value of a pseudorandom sample of a specified channel in a region of interest
 int getMaxChannelNearby(const Mat& roi, int channel, int iterations) {
 	int max = 0;
-	for (int i = 0; i < iterations; i++) {
-		int col = (i * 17) % roi.cols;
-		int row = (i * 41) % roi.rows;
-		int currentChannel = roi.at<Vec3b>(row, col)[channel];
+	for (int i = 0; i < iterations; i++) { //Iterations is sample size, adjusted based on time constraint
+		int col = (i * 17) % roi.cols; //Get pseudorandom column
+		int row = (i * 41) % roi.rows; //Get pseudorandom row
+		int currentChannel = roi.at<Vec3b>(row, col)[channel]; 
 		if (currentChannel > max) {
 			max = currentChannel;
 		}
@@ -56,21 +61,14 @@ bool linesAreSimilar(Vec4i line0, Vec4i line1, double angleThresh, int distanceT
 	double angle0 = angleOfLine(line0);
 	double angle1 = angleOfLine(line1);
 	if (angleThresh < abs(angle0 - angle1) && abs(angle0 - angle1) < CV_PI - angleThresh) return false;
-	//cout << line1[0] << " " << line1[1] << " " << line1[2] << " " << line1[3] << angle1 << endl;
-	//cout << line0[0] << " " << line0[1] << " " << line0[2] << " " << line0[3] << angle1 << endl;
 	int distance; //vertical or horizontal distance from line1, if it were extended forever, to first endpoint of line2
 	if (moreVerticalThanHorizontal(angle1)) { // if line1 is closer to vertical than horizontal
 		distance = ((cos(angle1) / sin(angle1)) * (line0[1] - line1[1]) + line1[0]) - line0[0];		//inverse function using point-slope form
-		//cout << "distance thresh: " << distanceThresh << " distance horizontal: " << distance << endl;
 	}
 	else {
 		double slope1 = (line1[3] - line1[1]) / (line1[2] - line1[0]);
-		//cout << "point " << (slope1 * (double)(line0[0] - line1[0]) - line1[1]) << endl;
-		//cout << "y value" << line0[1] << endl;
 		distance = (tan(angle1) * (line0[0] - line1[0]) + line1[1]) - line0[1]; //point-slope form where x0 is line1[0] and y0 is line1[1]
-		//cout << "distance vertical: " << distance << endl;
 	}
-	//cout << angle0 << endl;
 	return abs(distance) < distanceThresh;
 }
 
@@ -152,26 +150,26 @@ Vec4i getAvgHoughLine(vector<Vec4i> linesP) {
 	return meanLine;
 }
 
-
+//Determines if an angle is closer to being vertical than horizontal.
 bool moreVerticalThanHorizontal(double angle) {
 	return abs(abs(angle) - CV_PI / 2) < CV_PI / 4;
 }
 
+//Returns a cropped and scaled portion of a quadrant of an image so that it can be processed
 Mat getQuadrant(Mat& img, int quadrant) {
-	Mat roi;
+	Mat roi; //Region of interest
 	if (quadrant == 1) {
-		roi = img(Range(0, img.rows / 2), Range(img.cols / 2, img.cols - 1));
+		roi = img(Range(0, img.rows / 2), Range(img.cols / 2, img.cols - 1)); //Region of interest is quadrant 1
 	}
 	else if (quadrant == 2) {
-		roi = img(Range(0, img.rows / 2), Range(0, img.cols / 2));
+		roi = img(Range(0, img.rows / 2), Range(0, img.cols / 2));  //Region of interest is quadrant 2
 	}
 	else if (quadrant == 3) {
-		roi = img(Range(img.rows / 2, img.rows - 1), Range(0, img.cols / 2));
+		roi = img(Range(img.rows / 2, img.rows - 1), Range(0, img.cols / 2)); //Region of interest is quadrant 3
 	}
 	else {
-		roi = img(Range(img.rows / 2, img.rows - 1), Range(img.cols / 2, img.cols - 1));
+		roi = img(Range(img.rows / 2, img.rows - 1), Range(img.cols / 2, img.cols - 1)); //Region of interest is quadrant 4
 	}
-	//roi = roi(Range(roi.rows / 8, roi.rows / 10 * 9), Range(0, roi.cols / 10 * 7));
-	roi = roi(Range(roi.rows / 7, roi.rows * 6 / 7), Range(roi.cols / 7, roi.cols * 3 / 5));
+	roi = roi(Range(roi.rows / 7, roi.rows * 6 / 7), Range(roi.cols / 7, roi.cols * 3 / 5)); //Crop image
 	return roi;
 }
